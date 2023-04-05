1a675d222126770505e627d052a8253e76dbca02
KaraJH <karas.jones@yahoo.com>
2023-04-05 15:51:10 -0600

    test: Merchants show and index page

---
+++ spec/features/merchants/index_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe "Merchants Index" do 
+  before(:each) do 
+    visit merchants_path
+  end
+  describe " when visiting merchants" do 
+    it "has a list of merchants by name" do 
+
+      within(".merchants") do
+        expect(page).to have_content("Schroeder-Jerde")
+        expect(page).to have_css(".merch_name", count: 100)
+      end
+
+    end
+    it "has links to each merchant name that goes to the merchant show page" do 
+      within(".merchants") do 
+        expect(page).to have_link("Schroeder-Jerde")
+        click_link("Schroeder-Jerde")
+      end
+      expect(current_path).to eq("/merchants/1")
+    end
+  end
+end
